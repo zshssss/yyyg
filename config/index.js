7d044ad4b4cbf3b68e9a14b08b815f0a41ee1734
JobWebNie <1979510089@qq.com>
2018-08-30 12:48:16 +0800

    添加晒单预览

--- config/index.js
+++ config/index.js
@@ -15,12 +15,12 @@ module.exports = {
 
     // Various Dev Server settings
     // host: '192.168.1.105',// can be overwritten by process.env.HOST
-    host: '192.168.1.108', 
+    host: '192.168.1.111', 
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
 
     proxyTable: {
       '/apis': {
-        target: 'http://192.168.1.249/index.php/',//设置你调用的接口域名和端口号 别忘了加http
+        target: 'http://192.168.1.111/index.php/',//设置你调用的接口域名和端口号 别忘了加http
         changeOrigin: true,
         pathRewrite: {
           '^/apis': ''//这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
--- src/components/person/exportorder/exporseaction/exporseaction.vue
+++ src/components/person/exportorder/exporseaction/exporseaction.vue
@@ -14,12 +14,24 @@
     </div>
 
     <div class="addimg">
+        <ul class="img-container" >
+          <li class="img-item" v-if="item" v-for="(item,key) in srcUrls" :key="key">
+              <img class="bg-img" :src="item" alt="user image">
+              <a class="delete-item">
+                <span @click="handleDelete(key)">
+                  <i class="el-icon-delete"></i>
+                </span>
+              </a>
+          </li>
+        </ul>
         <div class="addbu">
             <p class="add">
-
+              
             </p>
-            <p class="name">图片</p>
+            <!-- <p class="name">图片</p> -->
+            <label for="fileupload">图片</label>
         </div>
+        <input type="file" @change="getFile" ref="fileupload" id="fileupload" multiple="multiple">
     </div>
 
     <p class="send_indo">发布</p>
@@ -31,7 +43,8 @@ export default {
   name: "exportorder",
   data() {
     return {
-      baseImgUrl: this.$store.state.baseImgUrl
+      baseImgUrl: this.$store.state.baseImgUrl,
+      srcUrls:[]
     };
   },
   created: function() {},
@@ -42,7 +55,23 @@ export default {
     },
     routerGo: function(path) {
       this.$router.push({ name: path });
-    }
+    },
+    getFile (e) {
+
+        let _this = this
+        var files = e.target.files[0]
+        if (!e || !window.FileReader) return;
+        var reader = new FileReader()
+        reader.readAsDataURL(files) // 转换
+        reader.onloadend = function () {
+          _this.srcUrls.push(this.result)
+          _this.$refs.fileupload.value = ''
+        }
+ 
+      },
+      handleDelete(index){
+        this.srcUrls.splice(index,1);
+      }
   }
 };
 </script>
