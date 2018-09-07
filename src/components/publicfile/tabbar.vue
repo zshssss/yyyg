db98c430f8c0145b63a9dd84b6644520eec13f4b
zshssss <18037472380@163.com>
2018-09-07 08:52:02 +0800

    shaohua首页修改

--- src/components/publicfile/tabbar.vue
+++ src/components/publicfile/tabbar.vue
@@ -2,32 +2,32 @@
   <div class="flex js_center al_center tc tabbar">
       <div class="item" v-bind:class="[nth==0?'on':'']" v-on:click="toRoute('index')">
          <div class="tab_ico_box">
-           <img :src="nth==0?baseImgUrl+'index_active_34_36.png':baseImgUrl+'index_default_34_36.png'" style="width:.68rem;height:.72rem;" alt="">
+           <img :src="nth==0?baseImgUrl+'index_default_34_36.png':baseImgUrl+'index_active_34_36.png'" style="width:.68rem;height:.72rem;" alt="">
          </div>
         <p>首页</p>
       </div>
        <div class="item" v-bind:class="[nth==1?'on':'']" v-on:click="toRoute('announce')">
          <div class="tab_ico_box">
-           <img :src="nth==1?baseImgUrl+'tabbar_2_42_42_active.png':baseImgUrl+'tabbar_2_42_42_default.png'" style="width:.84rem;height:.84rem;" alt="">
+           <img :src="nth==1?baseImgUrl+'tabbar_2_42_42_default.png':baseImgUrl+'tabbar_2_42_42_active.png'" style="width:.84rem;height:.84rem;" alt="">
          </div>
         <p>揭晓</p>
       </div>
        <div class="item" v-bind:class="[nth==2?'on':'']" v-on:click="toRoute('classify')">
          <div class="tab_ico_box">
-           <img :src="nth==2?baseImgUrl+'tabbar_3_42_42_avtive.png':baseImgUrl+'tabbar_3_42_42_default.png'" style="width:.84rem;height:.84rem;" alt="">
+           <img :src="nth==2?baseImgUrl+'tabbar_3_42_42_default.png':baseImgUrl+'tabbar_3_42_42_avtive.png'" style="width:.84rem;height:.84rem;" alt="">
          </div>
         <p>分类</p>
       </div>
        <div class="item" v-bind:class="[nth==3?'on':'']" v-on:click="toRoute('shop')">
         <div class="tab_ico_box">
-           <img :src="nth==3?baseImgUrl+'tabbar_4_40_38_active.png':baseImgUrl+'tabbar_4_40_38_default.png'" style="width:.80rem;height:.76rem;" alt="">
+           <img :src="nth==3?baseImgUrl+'tabbar_4_40_38_default.png':baseImgUrl+'tabbar_4_40_38_active.png'" style="width:.80rem;height:.76rem;" alt="">
         </div>
         <p>购物车</p>
       </div>
        <div class="item" v-bind:class="[nth==4?'on':'']" v-on:click="toRoute('person')">
          <div class="tab_ico_box">
-           <img :src="nth==4?baseImgUrl+'tabbar_5_36_42_active.png':baseImgUrl+'tabbar_5_36_42_default.png'" style="width:.72rem;height:.84rem;" alt="">
-         </div>
+           <img :src="nth==4?baseImgUrl+'tabbar_5_36_42_default.png':baseImgUrl+'tabbar_5_36_42_active.png'" style="width:.72rem;height:.84rem;" alt="">
+          </div>
         <p>我的</p>
       </div>
   </div>
